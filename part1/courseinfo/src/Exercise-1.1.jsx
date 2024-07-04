8b6296dc8d858f38c978e41cad7dfbd515b457a0
ADI <byadiexe@gmail.com>
2024-07-04 11:18:32 +0800

    Add Solution Exercise 1.2

--- part1/courseinfo/src/Exercise-1.1.jsx
+++ part1/courseinfo/src/Exercise-1.1.jsx
@@ -6,14 +6,6 @@ const Header = ({ course }) => {
   );
 };
 
-const Part = ({ part, exercises }) => {
-  return (
-    <p>
-      {part} {exercises}
-    </p>
-  );
-};
-
 const Content = ({
   part1,
   part2,
@@ -24,9 +16,15 @@ const Content = ({
 }) => {
   return (
     <div>
-      <Part part={part1} exercises={exercises1} />
-      <Part part={part2} exercises={exercises2} />
-      <Part part={part3} exercises={exercises3} />
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
     </div>
   );
 };
